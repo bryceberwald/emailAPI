3a4bb145df445b81326471d1676475c878a17be1
bryceberwald <bryce@berwald.io>
2023-05-31 01:45:18 -0700

    Started working with nodemailer npm package.

--- routes/email.js
+++ routes/email.js
@@ -1,10 +1,22 @@
 var express = require('express');
 var router = express.Router();
+const nodemailer = require("nodemailer");
 
 /* GET @ /email */
 router.get('/', function(req, res, next) {
+  
+  let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      type: 'OAuth2',
+      user: process.env.MY_EMAIL,
+      pass: process.env.MY_PASSWORD,
+    }
+  });
+
   res.json({"message": "Welcome to the email API @/email"});
   console.log("Welcome to the email API @/email");
+
 });
 
 module.exports = router;
